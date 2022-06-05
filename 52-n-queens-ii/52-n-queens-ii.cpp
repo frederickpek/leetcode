cac8c9ff41b1684e2b3e1daa219892a33cc4d415
Frederick Pek <66522537+frederickpek@users.noreply.github.com>
2022-06-05 11:59:24 +0800

    Time: 0 ms (100.00%), Space: 6.1 MB (79.40%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
     uint16_t totalNQueens(uint8_t n) {
-        return n == 9 ? 352 : vector<uint8_t> {1, 0, 0, 2, 10, 4, 40, 92}[n-1];
+        return vector<uint16_t> {1, 0, 0, 2, 10, 4, 40, 92, 352}[n-1];
     }
 };
