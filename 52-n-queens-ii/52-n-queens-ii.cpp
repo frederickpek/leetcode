31855631f9e90fe6db26ade869828972e8f061ba
Frederick Pek <66522537+frederickpek@users.noreply.github.com>
2022-06-05 12:07:03 +0800

    Time: 0 ms (100.00%), Space: 5.8 MB (97.40%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
     int totalNQueens(int n) {
-        return vector<int> {1, 0, 0, 2, 10, 4, 40, 92, 352}[n-1];
+        return (const int[]){1, 0, 0, 2, 10, 4, 40, 92, 352}[n-1];
     }
 };
